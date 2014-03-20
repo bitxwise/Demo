7877e55184da573af4b4a3d15915f61799fdc64d
bitxwise <bitxwise@gmail.com>
2014-03-20 16:29:03 -0600

    * Added SpecFlow package with feature definition using MSTest for testing

---
+++ DemoApplication.Specs/HelpUsersWriteSearchQueriesSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoApplication.Specs
+{
+    [Binding]
+    public class HelpUsersWriteSearchQueriesSteps
+    {
+        [Given(@"user is about to search")]
+        public void GivenUserIsAboutToSearch()
+        {
+            
+        }
+        
+        [When(@"user begins typing query")]
+        public void WhenUserBeginsTypingQuery()
+        {
+            
+        }
+        
+        [Then(@"a list of suggestions should appear below the search field")]
+        public void ThenAListOfSuggestionsShouldAppearBelowTheSearchField()
+        {
+            
+        }
+        
+        [Then(@"the suggestions should update as user types")]
+        public void ThenTheSuggestionsShouldUpdateAsUserTypes()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ DemoApplication.Specs/SearchHelp.feature
@@ -0,0 +1,11 @@
+﻿Feature: Help users write search queries
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add content assist to the Google Search field
+	Given user is about to search
+	When user begins typing query
+	Then a list of suggestions should appear below the search field
+	And the suggestions should update as user types

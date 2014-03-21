964fe152bc58d8631fe253ba1ffe06eac641006f
bitxwise <bitxwise@gmail.com>
2014-03-20 20:40:16 -0600

    DEMO-1 Testing 123 #time 5m

--- DemoApplication/Program.cs
+++ DemoApplication/Program.cs
@@ -9,7 +9,7 @@ namespace DemoApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I got it working!");
+            Console.WriteLine("I got it working! Sort of...still learning...");
             Console.WriteLine("Done");
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();

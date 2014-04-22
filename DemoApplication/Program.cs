15acd7659c8bbae7ea35bb91f834c95291807079
moeoro-rays <maurice.oro@rays.net>
2014-04-22 14:16:38 -0600

    FIRST!

--- DemoApplication/Program.cs
+++ DemoApplication/Program.cs
@@ -14,7 +14,7 @@ namespace DemoApplication
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
             Console.Beep();
-
+            Console.Beep();
         }
     }
 }
